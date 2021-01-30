c5dd2304f01cddd73561c338c4cb16075a042bdf
aspectie <kovleshenko99@mail.ru>
2021-01-30 15:33:01 +0300

    add hashrouter for gh

--- src/App.js
+++ src/App.js
@@ -7,19 +7,20 @@ import Home from "./components/home/Home";
 import './App.css';
 
 function App() {
-  return (
-      <div className="App">
-          <Switch>
-              <Route path='/home'
-                     render={() => <Home/>}/>
-              <Route path='/lostPass'
-                     render={() => <LostPass/>}/>
-              <Route path='/login'
-                     render={() => <Login/>}/>
-
-          </Switch>
-      </div>
-  );
+    return (
+        <div className="App">
+            <Switch>
+                <Route path='/home'
+                       render={() => <Home/>}/>
+                <Route path='/lostPass'
+                       render={() => <LostPass/>}/>
+                <Route path='/login'
+                       render={() => <Login/>}/>
+                <Route path='*'
+                       render={() => <div>404 NOT FOUND</div>}/>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import store from "./redux/redux-store";
 
 ReactDOM.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
         <Provider store={store}>
             <App/>
         </Provider>
-    </BrowserRouter>,
+    </HashRouter>,
 document.getElementById('root')
 );
